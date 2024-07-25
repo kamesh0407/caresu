42eb9afb455f18694e7da019647d446c48e09aec
kamesh0407 <kameshragupathi6@gmail.com>
2024-07-26 00:41:15 +0530

    PassCode verification completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PassKeyModal from "@/components/PassKeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home( {searchParams}: SearchParamProps ) {
+  const isAdmin = searchParams.admin === 'true';
+
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP Verification */}
+      {isAdmin && <PassKeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image src="/assets/icons/logo-full.svg" width={1000} height={1000} alt="Logo" className="mb-12 h-10 w-fit" />
