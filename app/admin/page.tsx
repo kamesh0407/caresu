2b00a0eef4f39e92e0c3a3cdbbeaf06abffa96a9
kamesh0407 <kameshragupathi6@gmail.com>
2024-07-26 23:51:18 +0530

    creating a table in admin dashboard

---
+++ app/admin/page.tsx
@@ -0,0 +1,86 @@
+import {DataTable} from '@/components/table/DataTable'
+import  StatCard  from '@/components/StatCard'
+import { getRecentAppointmentList } from '@/lib/actions/appointment.action'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import {columns, Payment} from '@/components/table/columns'
+
+async function getData(): Promise<Payment[]> {
+    // Fetch data from your API here.
+    return [
+      {
+        id: "728ed52f",
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+      },
+      // ...
+    ]
+  }
+
+const Admin = async () => {
+    const data = await getData()
+    const appointments = await getRecentAppointmentList();
+
+
+  return (
+    <div className='mx-auto flex max-w-7xl flex-col space-y-14'>
+        <header className='admin-header'>
+            <Link href="/" className='cursor-pointer'>
+            <Image
+                src="/assets/icons/logo-full.svg"
+                alt="logo"
+                width={32}
+                height={32}
+                className='h-8 w-fit' 
+            />
+            </Link>
+            <p className='text-16-semibold'>
+                Admin Dashboard
+            </p>
+
+        </header>
+        <main className='admin-main'>
+            <section className='w-full space-y-4'> 
+                <h1 className='header'>
+                    Welcome 👋🏻
+                </h1>
+                <p className='text-dark-700'>Start the day with managing new appointments</p>
+            </section>
+            <section className='admin-stat'>
+                    <StatCard
+                        type='appointments'
+                        count={appointments.scheduledCount}
+                        label='Scheduled appointments'
+                        icon='/assets/icons/appointments.svg'
+                    />
+                    <StatCard
+                        type='pending'
+                        count={appointments.pendingCount}
+                        label='pending appointments'
+                        icon='/assets/icons/pending.svg'
+                    />
+                    <StatCard
+                        type='cancelled'
+                        count={appointments.cancelledCount}
+                        label='cancelled appointments'
+                        icon='/assets/icons/cancelled.svg'
+                    />
+            </section>
+{/* 
+            <DataTable
+            coloumns={columns}
+                data={appointments.documents}
+            /> */}
+            <DataTable
+            columns={columns}
+                data={data}
+            />
+
+        </main>
+    </div>
+  )
+}
+
+export default Admin
